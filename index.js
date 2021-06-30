f07917822f258a842105ab18632ed8092be2868a
Aloha <utah.clayton@gmail.com>
2021-06-29 23:14:13 -0700

    added badge, working on adding license function. Gotta go

--- index.js
+++ index.js
@@ -9,7 +9,7 @@ const generateMarkdown = require('./utils/generateMarkdown')
 const questions = [
         {
             type: 'input',
-            name: 'name',
+            name: 'title',
             message: 'What is your name? (Required)',
             validate: nameInput => {
                 if (nameInput) {
@@ -64,12 +64,12 @@ const questions = [
 
 
 // TODO: Create a function to write README file
-function writeToFile(fileName, data) { //starter code
+function writeToFile(fileName, data) { 
     return fs.writeFileSync(path.join(process.cwd(), fileName), data);
 }
 
 // TODO: Create a function to initialize app
-function init() { //starter code
+function init() { 
     inquirer.prompt(questions).then((answers)=>{
         writeToFile("README.md", generateMarkdown({...answers}))
     });
